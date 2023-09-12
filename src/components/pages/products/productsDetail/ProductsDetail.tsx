16e7a7de1ee136f521b90753be04bb7ccbce0abf
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-09-12 14:48:41 +0300

    product details UI

--- src/components/pages/products/productsDetail/ProductsDetail.tsx
+++ src/components/pages/products/productsDetail/ProductsDetail.tsx
@@ -1,64 +1,69 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
-import { Box, Flex, Link } from "@chakra-ui/react";
-import { getProductsBySlug } from "../../../../../sanity/sanity";
-import Product from "./Product";
-import { IProduct } from "../../../../types";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { Box, Flex, Link } from '@chakra-ui/react'
+import { getProductsBySlug } from '../../../../../sanity/sanity'
+import Product from './Product'
+import { IProduct } from '../../../../types'
+import { useParams } from 'react-router-dom'
 
 const ProductsDetail: React.FC<IProduct> = () => {
-  const [products, setProducts] = useState<Array<IProduct>>([]);
-  const { slug } = useParams();
+  const [products, setProducts] = useState<Array<IProduct>>([])
+  const { slug } = useParams()
 
   useEffect(() => {
     const getData = async () => {
-      const allProducts = await getProductsBySlug();
-      setProducts(allProducts);
-    };
-    getData();
-  }, []);
+      const allProducts = await getProductsBySlug()
+      setProducts(allProducts)
+    }
+    getData()
+  }, [])
   return (
     <>
-      <Box>
-        <Link href="/">Go back</Link>
-      </Box>
+      <Flex flexDir={{ base: 'column' }} alignItems='center'>
+        <Box
+          width='88%'
+          textAlign='start'
+          mt={{ base: '20px' }}
+          mb={{ base: '30px' }}>
+          <Link href='/'>Go back</Link>
+        </Box>
 
-      <Flex
-        flexDir={{ base: "column" }}
-        width="100%"
-        height="100%"
-        alignItems="center"
-      >
-        {products
-          .filter((product) => product.slug.current == slug)
-          .map(
-            ({
-              title,
-              desc,
-              image,
-              slug,
-              price,
-              features,
-              box,
-              productImages,
-            }) => (
-              <Product
-                key={title}
-                title={title}
-                desc={desc}
-                image={image}
-                slug={slug}
-                price={price}
-                features={features}
-                box={box}
-                productImages={productImages}
-                products={products}
-              />
-            )
-          )}
+        <Flex
+          flexDir={{ base: 'column' }}
+          width='100%'
+          height='100%'
+          alignItems='center'>
+          {products
+            .filter((product) => product.slug.current == slug)
+            .map(
+              ({
+                title,
+                desc,
+                image,
+                slug,
+                price,
+                features,
+                box,
+                productImages,
+              }) => (
+                <Product
+                  key={title}
+                  title={title}
+                  desc={desc}
+                  image={image}
+                  slug={slug}
+                  price={price}
+                  features={features}
+                  box={box}
+                  productImages={productImages}
+                  products={products}
+                />
+              )
+            )}
+        </Flex>
       </Flex>
     </>
-  );
-};
+  )
+}
 
-export default ProductsDetail;
+export default ProductsDetail
