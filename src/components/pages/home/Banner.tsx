6ef03944ed9f47c528aeb7d3924988716cb85860
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-10-26 16:23:46 +0300

    commit

--- src/components/pages/home/Banner.tsx
+++ src/components/pages/home/Banner.tsx
@@ -1,78 +1,55 @@
-import { Box, Text, Heading, Link } from "@chakra-ui/react";
-
+import { Text, Heading, Link, Flex } from '@chakra-ui/react'
+import { seeProductButton } from './HomeStyles'
 const Banner = () => {
   return (
-    <Box
-      width="100%"
-      height="500px"
-      bg="black.200"
-      pt="95px"
-      display="flex"
-      justifyContent="center"
-      fontFamily="main.100"
-    >
-      <Box
-        width="88%"
-        display="flex"
-        flexDir="column"
-        alignItems="center"
-        px="15px"
-        textAlign="center"
-      >
+    <Flex
+      width='100%'
+      height='500px'
+      bg='black.200'
+      pt='95px'
+      justifyContent='center'
+      fontFamily='main.100'>
+      <Flex
+        width='88%'
+        flexDir={{ base: 'column' }}
+        alignItems='center'
+        px='15px'
+        textAlign='center'>
         <Heading
-          fontWeight="400"
-          color="white"
-          opacity="0.5"
-          textTransform="uppercase"
-          letterSpacing="10px"
-          mb="20px"
-          fontSize="14px"
-        >
+          fontWeight='400'
+          color='white'
+          opacity='0.5'
+          textTransform='uppercase'
+          letterSpacing='10px'
+          mb='20px'
+          fontSize={{ base: '14px' }}>
           New product
         </Heading>
         <Heading
-          color="white"
-          fontSize="36px"
-          textTransform="uppercase"
-          letterSpacing="1.4px"
-          mb="22px"
-          lineHeight="40px"
-        >
+          color='white'
+          fontSize='36px'
+          fontFamily='main.100'
+          textTransform='uppercase'
+          letterSpacing='1.4px'
+          mb='22px'
+          lineHeight='40px'>
           XX99 Mark II Headphones
         </Heading>
         <Text
-          color="white"
-          textAlign="center"
-          fontSize="15px"
-          mb="28px"
-          fontWeight="medium"
-          opacity="0.5"
-          lineHeight="25px"
-        >
+          color='white'
+          textAlign='center'
+          fontSize='15px'
+          mb='28px'
+          fontWeight='medium'
+          opacity='0.75'
+          lineHeight='25px'>
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </Text>
-        <Link
-          width="160px"
-          height="48px"
-          fontSize="13px"
-          fontWeight="bold"
-          fontFamily="main.100"
-          letterSpacing="1px"
-          bgColor="orange.100"
-          color="white.100"
-          textTransform="uppercase"
-          href="/headphones"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          cursor="pointer"
-        >
-          See product
-        </Link>
-      </Box>
-    </Box>
-  );
-};
+        <Link {...seeProductButton}>See product</Link>
+      </Flex>
+    </Flex>
+  )
+}
 
-export default Banner;
+export default Banner
--- src/components/pages/home/HomeStyles.tsx
+++ src/components/pages/home/HomeStyles.tsx
@@ -1,39 +1,41 @@
-import { BoxProps } from "@chakra-ui/react";
+import { BoxProps } from '@chakra-ui/react'
 
 // headings
 
 export const homeProductsHeadingStyle: BoxProps = {
-  fontSize: { base: "36px" },
-  fontWeight: "bold",
-  lineHeight: "40px",
-  fontFamily: "main.100",
-  letterSpacing: "1.3px",
-  color: "white.100",
-  textTransform: "uppercase",
-};
+  fontSize: { base: '36px' },
+  fontWeight: 'bold',
+  lineHeight: '40px',
+  fontFamily: 'main.100',
+  letterSpacing: '1.3px',
+  color: 'white.100',
+  textTransform: 'uppercase',
+}
 
 export const homeProductsHeadingStyle2: BoxProps = {
-  fontSize: { base: "28px" },
-  fontWeight: "bold",
-  lineHeight: "40px",
-  fontFamily: "main.100",
-  letterSpacing: "2px",
-  color: "black.100",
-  textTransform: "uppercase",
-};
+  fontSize: { base: '28px' },
+  fontWeight: 'bold',
+  lineHeight: '40px',
+  fontFamily: 'main.100',
+  letterSpacing: '2px',
+  color: 'black.100',
+  textTransform: 'uppercase',
+}
 // button
 
 export const seeProductButton: BoxProps = {
-  width: "160px",
-  height: "48px",
-  border: "1px solid black",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  cursor: "pointer",
-  textTransform: "uppercase",
-  fontSize: "13px",
-  fontWeight: "bold",
-  fontFamily: "main.100",
-  letterSpacing: "1px",
-};
+  width: '160px',
+  height: '48px',
+  border: '1px solid black',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  textTransform: 'uppercase',
+  fontSize: '13px',
+  fontWeight: 'bold',
+  fontFamily: 'main.100',
+  letterSpacing: '1px',
+  bgColor: 'orange.100',
+  color: 'white',
+}
