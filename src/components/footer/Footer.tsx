82e511d533ee11f2ad03a758658ac985555251ae
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-11-11 14:12:30 +0200

    footer UI base / md

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,10 +6,11 @@ const Footer = () => {
     <Flex
       bgColor="black.100"
       width="100%"
-      height={{ base: "654px" }}
+      height={{ base: "654px", md: "100%" }}
       flexDir={{ base: "column" }}
       color="white"
-      alignItems="center"
+      alignItems={{ base: "center", md: "start" }}
+      px={{ md: "25px" }}
     >
       <Flex
         width={{ base: "104px" }}
@@ -20,11 +21,11 @@ const Footer = () => {
       <Image src="/public/shared/desktop/logo.svg" />
 
       <Flex
-        flexDir={{ base: "column" }}
+        flexDir={{ base: "column", md: "row" }}
         mt={{ base: "50px" }}
         textAlign="center"
-        width={{ base: "108px" }}
-        height={{ base: "148px" }}
+        width={{ base: "100%", md: "429px" }}
+        height={{ base: "148px", md: "22px" }}
         justifyContent="space-between"
       >
         <Link href="/" {...FooterLinkStyle}>
@@ -43,10 +44,10 @@ const Footer = () => {
 
       <Flex
         width={{ base: "100%" }}
-        height={{ base: "150px" }}
+        height={{ base: "100%" }}
         mt={{ base: "50px" }}
-        textAlign="center"
-        px="25px"
+        textAlign={{ base: "center", md: "start" }}
+        px={{ base: "25px", md: "0px" }}
       >
         <Text
           fontSize={{ base: "15px" }}
@@ -61,33 +62,41 @@ const Footer = () => {
           demo facility - we’re open 7 days a week.
         </Text>
       </Flex>
-      <Text
-        mt={{ base: "50px" }}
-        fontSize={{ base: "15px" }}
-        fontFamily="main.100"
-        fontWeight="medium"
-        lineHeight="25px"
-        opacity="0.5"
-      >
-        Copyright 2021. All Rights Reserved
-      </Text>
 
       <Flex
-        mt={{ base: "48px" }}
-        mb={{ base: "38px" }}
-        width={{ base: "104px" }}
-        height={{ base: "24px" }}
-        justifyContent="space-between"
+        width={{ base: "100%" }}
+        flexDir={{ base: "column", md: "row" }}
+        alignItems="center"
+        justifyContent={{ md: "space-between" }}
       >
-        <Link href="https://facebook.com" isExternal>
-          <Image src="/public/shared/desktop/icon-facebook.svg" />
-        </Link>
-        <Link href="https://twitter.com" isExternal>
-          <Image src="/public/shared/desktop/icon-twitter.svg" />
-        </Link>
-        <Link href="https://instagram.com" isExternal>
-          <Image src="/public/shared/desktop/icon-instagram.svg" />
-        </Link>
+        <Text
+          mt={{ base: "50px", md: "0px" }}
+          fontSize={{ base: "15px" }}
+          fontFamily="main.100"
+          fontWeight="medium"
+          lineHeight="25px"
+          opacity="0.5"
+        >
+          Copyright 2021. All Rights Reserved
+        </Text>
+
+        <Flex
+          mt={{ base: "48px" }}
+          mb={{ base: "38px" }}
+          width={{ base: "104px" }}
+          height={{ base: "24px" }}
+          justifyContent="space-between"
+        >
+          <Link href="https://facebook.com" isExternal>
+            <Image src="/public/shared/desktop/icon-facebook.svg" />
+          </Link>
+          <Link href="https://twitter.com" isExternal>
+            <Image src="/public/shared/desktop/icon-twitter.svg" />
+          </Link>
+          <Link href="https://instagram.com" isExternal>
+            <Image src="/public/shared/desktop/icon-instagram.svg" />
+          </Link>
+        </Flex>
       </Flex>
     </Flex>
   );
