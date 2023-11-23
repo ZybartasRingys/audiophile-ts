ccecb72cd1309833ddaa247fe111e4461fd9f34d
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-11-23 22:57:17 +0200

    commit

--- src/components/pages/home/Banner.tsx
+++ src/components/pages/home/Banner.tsx
@@ -1,33 +1,38 @@
-import { Text, Heading, Link, Flex } from "@chakra-ui/react";
+import { Text, Heading, Link, Flex, Image } from "@chakra-ui/react";
 import { Default1btn } from "../../../chakra/appStyles";
 
 const Banner = () => {
   return (
     <Flex
       width="100%"
-      height={{ base: "31.81rem", md: "39.8125rem" }}
+      height={{ base: "31.81rem", md: "39.8125rem", lg: "39.5rem" }}
       mb={{ base: "1.4375rem" }}
-      justifyContent="center"
+      justifyContent={{ base: "center", lg: "start" }}
       alignItems="center"
       fontFamily="main.100"
+      bgColor="black.300"
       bgImg={{
         base: "/home/mobile/image-header.jpg",
         md: "home/tablet/image-header.jpg",
+        lg: "",
       }}
       backgroundPosition="center"
       bgSize="cover"
       bgRepeat="no-repeat"
-      bgPos="center -5.625rem"
+      bgPos={{ base: "center -5.625rem", lg: "center" }}
+      border="1px solid red"
+      px={{ lg: "10.3125rem" }}
     >
       <Flex
-        width={{ base: "100%", md: "50%" }}
+        width={{ base: "100%", md: "50%", lg: "100%" }}
+        maxW={{ lg: "24.875rem" }}
         height={{ base: "100%", md: "21.625rem" }}
         flexDir={{ base: "column" }}
-        alignItems="center"
-        justifyContent="center"
-        px="1rem"
+        alignItems={{ base: "center", lg: "start" }}
+        justifyContent={{ base: "center" }}
+        px={{ base: "1rem", lg: "0" }}
         pt={{ base: "5.9375rem", md: "0" }}
-        textAlign="center"
+        textAlign={{ base: "center", lg: "start" }}
       >
         <Heading
           fontFamily="main.100"
@@ -54,7 +59,7 @@ const Banner = () => {
         </Heading>
         <Text
           color="white.100"
-          textAlign="center"
+          textAlign={{ base: "center", lg: "start" }}
           fontSize=".9375rem"
           mb="1.75rem"
           fontWeight="medium"
@@ -65,6 +70,9 @@ const Banner = () => {
           for the passionate music enthusiast.
         </Text>
         <Link {...Default1btn}>See product</Link>
+      </Flex>
+      <Flex width="100%" height="100%">
+        <Image src="/home/tablet/image-header.jpg" />
       </Flex>
     </Flex>
   );
