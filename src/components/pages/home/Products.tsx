f8313acee8e6e404775b44cbea1244e8bf206557
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-11-26 21:43:07 +0200

    zx9 lg

--- src/components/pages/home/Products.tsx
+++ src/components/pages/home/Products.tsx
@@ -19,67 +19,76 @@ const Products = () => {
     >
       <Flex
         width={{ base: "100%" }}
-        height={{ base: "37.5rem", md: "45rem" }}
+        height={{ base: "37.5rem", md: "45rem", lg: "35rem" }}
         pt={{ md: ".625rem" }}
-        flexDir={{ base: "column" }}
-        alignItems="center"
+        pl={{ lg: "9.125rem" }}
+        flexDir={{ base: "column", lg: "row" }}
+        alignItems={{ base: "center", lg: "start" }}
+        justifyContent={{ lg: "start" }}
         overflow="hidden"
         borderRadius="8px"
         bgColor="orange.100"
         bgImg={{ base: "/home/desktop/pattern-circles.svg" }}
         bgRepeat="no-repeat"
-        bgSize={{ base: "173%", md: "126%" }}
-        bgPos={{ base: "center -7.6875rem", md: "center -15rem" }}
+        bgSize={{ base: "173%", md: "126%", lg: "88%" }}
+        bgPos={{
+          base: "center -7.6875rem",
+          md: "center -15rem",
+          lg: "top -4rem left -9rem ",
+        }}
       >
         <Image
           position="relative"
-          width={{ base: "10.625rem", md: "11.875rem" }}
-          height={{ base: "12.9375rem", md: "13.625rem" }}
-          src="/home/mobile/image-speaker-zx9.png"
+          width={{ base: "10.625rem", md: "11.875rem", lg: "25rem" }}
+          height={{ base: "12.9375rem", md: "13.625rem", lg: "30.8125rem" }}
+          src="/home/desktop/image-speaker-zx9.png"
           alt="ZX9"
           loading="lazy"
           mt={{ base: "3.4375rem" }}
+          mr={{ lg: "9.4375rem" }}
         ></Image>
         <Flex
-          width={{ base: "86%", md: "349px" }}
+          width={{ base: "86%", md: "21.8125rem" }}
           flexDir={{ base: "column" }}
-          textAlign="center"
-          pt={{ base: "2.25rem", md: "4rem" }}
-          mb={{ base: "1.6rem", md: "1.875rem" }}
+          textAlign={{ base: "center" }}
+          alignItems={{ base: "center", lg: "start" }}
+          pt={{ base: "2.25rem", md: "4rem", lg: "0" }}
+          border="1px solid red"
         >
           <Text {...homeProductsHeadingStyle}>ZX9</Text>
           <Text {...homeProductsHeadingStyle}>speaker</Text>
           <Text
-            textAlign="center"
-            fontSize="15px"
-            lineHeight="25px"
+            textAlign={{ base: "center", lg: "start" }}
+            fontSize=".9375rem"
+            lineHeight="1.5625rem"
             opacity="0.75"
             color="white.100"
             mt={{ base: "1.625rem", md: "1.25rem" }}
+            mb={{ base: "1.6rem", md: "1.875rem" }}
           >
             Upgrade to premium speakers that are phenomenally built to deliver
             truly remarkable sound.
           </Text>
-        </Flex>
 
-        <Link
-          width="160px"
-          height="48px"
-          borderRadius="none"
-          color="white.100"
-          bgColor="black.200"
-          textTransform="uppercase"
-          fontSize="13px"
-          fontFamily="main.100"
-          letterSpacing="1px"
-          fontWeight="bold"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          href="/speakers"
-        >
-          See product
-        </Link>
+          <Link
+            width="160px"
+            height="48px"
+            borderRadius="none"
+            color="white.100"
+            bgColor="black.200"
+            textTransform="uppercase"
+            fontSize="13px"
+            fontFamily="main.100"
+            letterSpacing="1px"
+            fontWeight="bold"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            href="/speakers"
+          >
+            See product
+          </Link>
+        </Flex>
       </Flex>
 
       {/* ZX 7 SPEAKER */}
