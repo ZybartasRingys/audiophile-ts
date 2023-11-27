311f96adb01f5c69022aa5980a988baf0c608729
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2023-11-27 22:25:13 +0200

    testimonial base / md / lg

--- src/chakra/theme.tsx
+++ src/chakra/theme.tsx
@@ -1,5 +1,5 @@
 (""); // 1. Import the extendTheme function
-import { StyleFunctionProps, extendTheme } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import "@fontsource-variable/manrope";
 
 // 2. Extend the theme to include custom colors, fonts, etc
--- src/components/pages/home/Testimonial.tsx
+++ src/components/pages/home/Testimonial.tsx
@@ -1,54 +1,64 @@
-import { Flex, Box, Text, Heading } from '@chakra-ui/react'
-
+import { Flex, Box, Text, Heading } from "@chakra-ui/react";
+import "../../../App.css";
 const Testimonial = () => {
   return (
     <Flex
-      flexDir={{ base: 'column' }}
-      width='100%'
-      height={{ base: '100%' }}
-      mt={{ base: '7.5rem', md: '6rem' }}
-      mb={{ base: '7.5rem', md: '96px' }}
-      alignItems='center'
-      textAlign='center'
-      fontFamily='main.100'>
+      flexDir={{ base: "column", lg: "row-reverse" }}
+      width="100%"
+      height={{ base: "100%", lg: " 36.75rem" }}
+      mt={{ base: "7.5rem", md: "6rem" }}
+      mb={{ base: "7.5rem", md: "96px" }}
+      alignItems="center"
+      justifyContent={{ lg: "space-between" }}
+      textAlign="center"
+      fontFamily="main.100"
+    >
       <Box
-        width={{ base: '100%', md: '100%' }}
-        height={{ base: '18.75rem' }}
+        width={{ base: "100%", md: "100%", lg: "50%" }}
+        height={{ base: "18.75rem", lg: "36.75rem" }}
         bgImg={{
-          base: '/shared/mobile/image-best-gear.jpg',
-          md: '/shared/tablet/image-best-gear.jpg',
+          base: "/shared/mobile/image-best-gear.jpg",
+          md: "/shared/tablet/image-best-gear.jpg",
+          lg: "/shared/desktop/image-best-gear.jpg",
         }}
-        bgPos='center'
-        bgSize='cover'
-        bgRepeat='no-repeat'
-        borderRadius='8'
-        mb='2.68rem'></Box>
+        bgPos="center"
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        borderRadius="8"
+        mb="2.68rem"
+      ></Box>
 
       <Flex
-        flexDir={{ base: 'column' }}
-        width={{ base: '100%', md: '35.81rem' }}>
-        <Heading
-          fontSize={{ base: '1.75rem', md: '2.5rem' }}
-          letterSpacing='1px'
-          textTransform='uppercase'
-          fontWeight='bold'
-          fontFamily='main.100'
-          mb='2rem'
-          color='black.200'
-          lineHeight={{ md: '2.75rem' }}>
-          Bringing you the
-          <Text as='span' color='orange.100' textTransform='uppercase'>
-            best
-          </Text>
-          audio gear
-        </Heading>
+        flexDir={{ base: "column" }}
+        width={{ base: "100%", md: "35.81rem", lg: "27.8125rem" }}
+        alignItems={{ base: "center" }}
+      >
+        <Flex width={{ base: "85%", md: "100%" }} textAlign={{ lg: "start" }}>
+          <Heading
+            fontSize={{ base: "1.75rem", md: "2.5rem" }}
+            letterSpacing="1px"
+            textTransform="uppercase"
+            fontWeight="bold"
+            fontFamily="main.100"
+            mb="2rem"
+            color="black.200"
+            lineHeight={{ md: "2.75rem" }}
+          >
+            Bringing you the
+            <span className="best"> best </span>
+            audio gear
+          </Heading>
+        </Flex>
+
         <Text
-          fontSize={{ base: '0.937rem' }}
-          lineHeight='1.56rem'
-          fontFamily='main.100'
-          fontWeight='medium'
-          opacity='0.5'
-          color='black.200'>
+          fontSize={{ base: "0.937rem" }}
+          lineHeight="1.56rem"
+          fontFamily="main.100"
+          fontWeight="medium"
+          opacity="0.5"
+          color="black.200"
+          textAlign={{ lg: "start" }}
+        >
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
@@ -58,7 +68,7 @@ const Testimonial = () => {
         </Text>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
