7cfdd212f570ce65817b8128797ea67eeaabb8d8
DevZybartas <153736758+DevZybartas@users.noreply.github.com>
2024-01-12 09:43:23 +0200

    routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './chakra/theme.tsx'
-import { CartContextProvider } from './context/CartContext.tsx'
-import { AppContextProvider } from './context/AppContext.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./chakra/theme.tsx";
+import { CartContextProvider } from "./context/CartContext.tsx";
+import { AppContextProvider } from "./context/AppContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppContextProvider>
       <CartContextProvider>
@@ -19,4 +19,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </CartContextProvider>
     </AppContextProvider>
   </React.StrictMode>
-)
+);
